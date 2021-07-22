fb403fc33e12743ecd9f17fdb978512a1d182774
goN <contato@arielton.com>
2021-07-22 05:11:40 -0300

    Update converter_plays.bat

--- converter_plays.bat
+++ converter_plays.bat
@@ -1,5 +1,4 @@
 SET "plays=%userprofile%\Documents\League of Legends\Highlights"
 CD %plays%
 FORFILES /S /M *.webm /C "CMD /C ren @file @file.mp4"
-CD %userprofile%\Documents\www\converter\simple_version
 IF %ERRORLEVEL% NEQ 0 Echo An error was found
